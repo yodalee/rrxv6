f1a9f6760ab3eb9568bf52bf0071979755841e24
yodalee <lc85301@gmail.com>
2021-10-16 16:47:02 +0800

    add trampoline code for user space

--- build.rs
+++ build.rs
@@ -16,6 +16,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Build::new()
         .file("src/entry.S")
         .file("src/kernelvec.S")
+        .file("src/trampoline.S")
         .compile("asm");
 
     // rebuild if `entry.s` changed
