c8e5bfdea55d9ff339e1d985c8f85ba3bae63e14
yodalee <lc85301@gmail.com>
2021-12-14 23:36:19 +0800

    add PhysAddr of u64 abstract

--- src/vm/addr.rs
+++ src/vm/addr.rs
@@ -8,6 +8,13 @@ pub struct InvalidVirtAddr;
 #[derive(Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
 pub struct VirtAddr(u64);
 
+/// A 64-bits physical memory address.
+///
+/// A wrapper type around `u64`
+/// On riscv, only lower 56 bits can be used, top 8 bits must be zeroed.
+#[derive(Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
+pub struct PhysAddr(u64);
+
 impl VirtAddr {
     #[inline]
     pub fn new(addr: u64) -> Self {
@@ -108,6 +115,119 @@ impl Sub<usize> for VirtAddr {
 }
 
 impl SubAssign<usize> for VirtAddr {
+    #[inline]
+    fn sub_assign(&mut self, rhs: usize) {
+        self.sub_assign(rhs as u64);
+    }
+}
+
+#[derive(Debug)]
+pub struct InvalidPhysAddr;
+
+impl PhysAddr {
+    #[inline]
+    pub fn new(addr: u64) -> Self {
+        Self::try_new(addr).expect(&format!("Physical address in riscv should have bit 56-63 zeroed {}", addr))
+    }
+
+    /// Try to create a new physical address.
+    #[inline]
+    pub fn try_new(addr: u64) -> Result<PhysAddr, InvalidPhysAddr> {
+        match addr.get_bits(56..64) {
+            0 => Ok(PhysAddr(addr)),   // valid address
+            _ => Err(InvalidPhysAddr{}),
+        }
+    }
+
+    /// Create a PhysAddr with zeroed bit 56-64
+    #[inline]
+    pub fn new_truncate(addr: u64) -> Self {
+        Self(addr & ((1 << 56) - 1))
+    }
+
+    #[inline]
+    pub fn as_u64(self) -> u64 {
+        self.0
+    }
+
+    #[inline]
+    pub fn as_pte(self) -> u64 {
+        (self.0 >> 12) << 10
+    }
+
+    #[inline]
+    pub fn align_down(self) -> Self {
+        Self(
+            align_down(self.0, 4096)
+        )
+    }
+
+    #[inline]
+    pub fn align_up(self) -> Self {
+        Self(
+            align_up(self.0, 4096)
+        )
+    }
+}
+
+impl Add<u64> for PhysAddr {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, rhs: u64) -> Self::Output {
+        PhysAddr::new(self.0 + rhs)
+    }
+}
+
+impl AddAssign<u64> for PhysAddr {
+    #[inline]
+    fn add_assign(&mut self, rhs: u64) {
+        *self = *self + rhs;
+    }
+}
+
+impl Add<usize> for PhysAddr {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, rhs: usize) -> Self::Output {
+        self + rhs as u64
+    }
+}
+
+impl AddAssign<usize> for PhysAddr {
+    #[inline]
+    fn add_assign(&mut self, rhs: usize) {
+        self.add_assign(rhs as u64)
+    }
+}
+
+impl Sub<u64> for PhysAddr {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, rhs: u64) -> Self::Output {
+        PhysAddr::new(self.0 - rhs)
+    }
+}
+
+impl SubAssign<u64> for PhysAddr {
+    #[inline]
+    fn sub_assign(&mut self, rhs: u64) {
+        *self = *self - rhs;
+    }
+}
+
+impl Sub<usize> for PhysAddr {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, rhs: usize) -> Self::Output {
+        self - rhs as u64
+    }
+}
+
+impl SubAssign<usize> for PhysAddr {
     #[inline]
     fn sub_assign(&mut self, rhs: usize) {
         self.sub_assign(rhs as u64);
