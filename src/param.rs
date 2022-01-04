2db7b67a8dd537aa0ca9ab7a5560153d777dafff
yodalee <lc85301@gmail.com>
2022-01-04 23:59:29 +0800

    add Proc struct and static array

--- src/param.rs
+++ src/param.rs
@@ -4,3 +4,4 @@ pub const STACK_SIZE: usize = 4096;
 pub const OS_STACK_SIZE: usize = 8192;
 pub const UART_TX_BUF_SIZE: usize = 32;
 pub const CONSOLE_BUF_SIZE: usize = 128;
+pub const LEN_PROCNAME: usize = 16;
--- src/proc.rs
+++ src/proc.rs
@@ -1,4 +1,39 @@
 //! kernel process table
 
+use crate::param::{NPROC, LEN_PROCNAME};
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+/// Process state
+#[derive(Eq,PartialEq)]
+pub enum ProcState {
+    RUNNABLE,
+    RUNNING,
+}
+
+pub struct Proc {
+    state: ProcState,
+    kstack: u64,
+    name: [char;LEN_PROCNAME],
+}
+
+impl Proc {
+    pub const fn new() -> Self {
+        Self {
+            state: ProcState::RUNNABLE,
+            kstack: 0,
+            name: ['\0';LEN_PROCNAME],
+        }
+    }
+}
+
+lazy_static! {
+    pub static ref PROC: [Mutex<Proc>;NPROC] = {
+        const INIT_PROC: Mutex<Proc> = Mutex::new(Proc::new());
+        [INIT_PROC;NPROC]
+    };
+}
+
+/// initialize kernel process stack
 pub fn init_proc() {
 }
