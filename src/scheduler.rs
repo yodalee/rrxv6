ab58b041d6f9ca70a443d0397dbff901dfed0b45
yodalee <lc85301@gmail.com>
2022-03-23 00:51:55 +0800

    add used process list and spawn in scheduler

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,21 +1,41 @@
 use crate::list::List;
-use crate::proc::Proc;
+use crate::proc::{Proc, ProcState, get_pid};
+use crate::riscv::PAGESIZE;
 use alloc::boxed::Box;
+use spin::Mutex;
 
 static mut SCHEDULER: Option<Scheduler> = None;
 
 pub struct Scheduler {
+    pub used: Mutex<List<Box<Proc>>>,
     pub unused: List<Box<Proc>>
 }
 
 impl Scheduler {
     fn new() -> Self {
         Self {
+            used: Mutex::new(List::new()),
             unused: List::new(),
         }
     }
 
-    pub fn spawn(&self) {
+    pub fn spawn(&mut self, f: u64) {
+        match self.unused.pop() {
+            Some(mut proc) => {
+                // initialize process
+                proc.pid = get_pid();
+                proc.state = ProcState::RUNNABLE;
+                proc.context.reset();
+                proc.context.ra = f;
+                proc.context.sp = proc.kstack + PAGESIZE;
+
+                let mut used_list = self.used.lock();
+                used_list.push(proc);
+            },
+            None => {
+                panic!("No unused process left");
+            }
+        }
     }
 
     pub fn schedule(&self) -> ! {
