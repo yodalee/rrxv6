81a5f2db797001c561a39782d56db9c1ec48cd70
Yoda Lee <lc85301@gmail.com>
2022-05-12 21:46:37 +0800

    first syscall syscall_write of print data
    Wait println

--- src/syscall.rs
+++ src/syscall.rs
@@ -1,4 +1,28 @@
+use crate::cpu::get_proc;
+use lazy_static::lazy_static;
+
+const SYSCALL_NUM : usize = 1;
+type SyscallEntry = fn() -> u64;
+lazy_static! {
+    static ref SYSCALLS: [SyscallEntry;SYSCALL_NUM] = [
+        syscall_write
+    ];
+}
+
+fn syscall_write() -> u64 {
+    0
+}
 
 pub fn syscall() {
-    unimplemented!();
+    unsafe {
+        let proc = get_proc();
+        let trapframe = (*proc).trapframe.as_mut();
+        let syscall_id : usize = trapframe.a7 as usize;
+
+        trapframe.a0 = if syscall_id < SYSCALLS.len() {
+            SYSCALLS[syscall_id]()
+        } else {
+            u64::MAX
+        }
+    }
 }
