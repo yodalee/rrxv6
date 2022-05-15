3ff97c0f1d098ee70aac537050afb0f8826f182d
Yoda Lee <lc85301@gmail.com>
2022-05-15 15:46:52 +0800

    implement get_arg and print char from syscall

--- src/syscall.rs
+++ src/syscall.rs
@@ -1,4 +1,5 @@
 use crate::cpu::get_proc;
+use crate::println;
 use lazy_static::lazy_static;
 
 const SYSCALL_NUM : usize = 1;
@@ -9,7 +10,28 @@ lazy_static! {
     ];
 }
 
+enum ArgIndex {
+    A0, A1, A2, A3, A4, A5,
+}
+
+/// get u64 raw value store in trapframe->a0 to trapframe->a6
+fn get_arg(n: ArgIndex) -> u64 {
+    let proc = get_proc();
+    let trapframe = unsafe { (*proc).trapframe.as_mut() };
+    match n {
+        ArgIndex::A0 => trapframe.a0,
+        ArgIndex::A1 => trapframe.a1,
+        ArgIndex::A2 => trapframe.a2,
+        ArgIndex::A3 => trapframe.a3,
+        ArgIndex::A4 => trapframe.a4,
+        ArgIndex::A5 => trapframe.a5,
+    }
+}
+
 fn syscall_write() -> u64 {
+    let c = get_arg(ArgIndex::A1);
+    let c = char::from_u32(c as u32).unwrap();
+    println!("{}", c);
     0
 }
 
