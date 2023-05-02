f3e706f288579a4501ad135dfe2a2a91e2a3e25b
Yoda Lee <lc85301@gmail.com>
2023-05-02 22:09:40 +0800

    Use mutex to protect disk accessment

--- src/disk.rs
+++ src/disk.rs
@@ -2,21 +2,22 @@ use crate::virtio::header::VirtioHeader;
 use crate::virtio::block::VirtioBlock;
 use crate::memorylayout::VIRTIO0;
 
-static mut DISK: Option<VirtioBlock> = None;
+use spin::Mutex;
+
+static mut DISK: Mutex<Option<VirtioBlock>> = Mutex::new(None);
 
 pub fn init_disk() {
     let header = unsafe { &mut *(VIRTIO0 as *mut VirtioHeader) };
     let block = VirtioBlock::new(header);
     match block {
         Ok(block) => unsafe {
-            DISK = Some(block);
+            let mut disk = DISK.lock();
+            *disk = Some(block);
         },
         Err(_err) => panic!("Error: Disk initialization"),
     }
 }
 
-pub fn get_disk() -> &'static mut VirtioBlock {
-    unsafe {
-        DISK.as_mut().unwrap()
-    }
+pub fn read_disk() {
+    let disk = unsafe { DISK.lock().as_mut().unwrap() };
 }
