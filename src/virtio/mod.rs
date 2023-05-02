55ac89d143f5e9452435c4d9b0fb00601e40b8b3
Yoda Lee <lc85301@gmail.com>
2023-05-02 22:09:40 +0800

    MOD: enum Error

--- src/virtio/mod.rs
+++ src/virtio/mod.rs
@@ -1,2 +1,16 @@
 pub mod block;
 pub mod header;
+
+#[derive(Debug)]
+pub enum Error {
+    /// Device status not OK
+    HeaderInitError,
+    /// The queue is already in use.
+    AlreadyUsed,
+    /// The queue is not available
+    NotAvailable,
+    /// Invalid arguments
+    InvalidArguments,
+    /// Cannot get memory
+    NoMemory,
+}
